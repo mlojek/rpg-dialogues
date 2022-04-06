c61033546ed48e6ba70c2b435ae18df8a1e7ee59
Marcin <marcinlojek14@gmail.com>
2022-04-06 15:28:18 +0200

    Add definitions for Dialogue methods

--- src/dialogue.cpp
+++ src/dialogue.cpp
@@ -7,9 +7,33 @@ Dialogue::Dialogue(std::string headMessage) {
     reset();
 }
 
+int Dialogue::getNodeCount() {
+    return nodes_.size();
+}
+
+int Dialogue::getChoiceCount() {
+    return current_->getChoiceCount();
+}
+
+std::string Dialogue::getMessage() {
+    return current_->getMessage();
+}
+
+std::vector<std::string> Dialogue::getChoiceMessages() {
+    return current_->getChoiceMessages();
+}
+
+bool Dialogue::isDone() {
+    return current_ == nullptr;
+}
+
 int Dialogue::addNode(std::string message) {
     nodes_.push_back(Node(message));
     return nodes_.size() - 1;
+}
+
+void Dialogue::makeChoice(unsigned choiceNo) {
+    current_ = current_->getNext(choiceNo);
 }
 
 void Dialogue::reset() {
