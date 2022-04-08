11c6bb34c4419980a98c06a5063b518b7f8fabcd
Marcin <marcinlojek14@gmail.com>
2022-04-08 13:10:55 +0200

    Add testDialogue.hpp

--- tests/runTests.cpp
+++ tests/runTests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "testNode.hpp"
+#include "testDialogue.hpp"
 
 
 int main(int argc, char **argv) {
---
+++ tests/testDialogue.hpp
@@ -0,0 +1,9 @@
+#ifndef TEST_DIALOGUE_HPP
+#define TEST_DIALOGUE_HPP
+
+#include <gtest/gtest.h>
+#include "../src/dialogue.hpp"
+
+
+
+#endif
