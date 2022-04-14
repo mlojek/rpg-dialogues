ffce4b4757c6bdb110977f4981b9a7a570663e43
Marcin <marcinlojek14@gmail.com>
2022-04-14 10:51:57 +0200

    Add testDialogue::testConstructor

--- tests/testDialogue.hpp
+++ tests/testDialogue.hpp
@@ -5,5 +5,13 @@
 #include "../src/dialogue.hpp"
 
 
+TEST(testDialogue, testConstructor) {
+    Dialogue testDialogue = Dialogue("Head message", "speaker");
+
+    ASSERT_EQ(testDialogue.getNodeCount(), 1);
+    ASSERT_EQ(testDialogue.getMessage(), "Head message");
+    ASSERT_EQ(testDialogue.getSpeaker(), "speaker");
+}
+
 
 #endif
