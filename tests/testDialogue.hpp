e678d16dde667439b731983f1aaf441eec3214cc
Marcin <marcinlojek14@gmail.com>
2022-04-15 10:34:43 +0200

    Add new Dialogue class unit tests

--- tests/testDialogue.hpp
+++ tests/testDialogue.hpp
@@ -14,6 +14,31 @@ TEST(testDialogue, testConstructor) {
     ASSERT_EQ(testDialogue.getNodeCount(), 1);
     ASSERT_EQ(testDialogue.getMessage(), "Head message");
     ASSERT_EQ(testDialogue.getSpeaker(), "speaker");
+    ASSERT_EQ(testDialogue.printMessage(), "speaker: Head message");
+    ASSERT_EQ(testDialogue.getChoiceCount(), 0);
+}
+
+TEST(testDialogue, testAddNodeAndGetNodeCount) {
+    Dialogue testDialogue = Dialogue("Head message", "speaker");
+    testDialogue.addNode("New message", "new speaker");
+    testDialogue.addNode("Other message", "other speaker");
+
+    ASSERT_EQ(testDialogue.getNodeCount(), 3);
+}
+
+TEST(testDialogue, testAddNodeAndGetIndex) {
+    Dialogue testDialogue = Dialogue("Head message", "speaker");
+    ASSERT_EQ(testDialogue.addNode("New message", "new speaker"), 1);
+    ASSERT_EQ(testDialogue.addNode("Other message", "other speaker"), 2);
+}
+
+TEST(testDialogue, testLinkNodes) {
+    Dialogue testDialogue = Dialogue("Head message", "speaker");
+    testDialogue.addNode("New message", "new speaker");
+
+    testDialogue.linkNodes(0, 1);
+
+    ASSERT_EQ(testDialogue.getChoiceCount(), 1);
 }
 
 
