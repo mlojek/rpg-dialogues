9335b556db06245acf03ed4584251bb274d81487
Marcin <marcinlojek14@gmail.com>
2022-04-15 10:44:32 +0200

    Add further Dialogue class unit tests

--- tests/testDialogue.hpp
+++ tests/testDialogue.hpp
@@ -2,6 +2,8 @@
 #define TEST_DIALOGUE_HPP
 
 #include <gtest/gtest.h>
+#include <string>
+#include <vector>
 #include "../include/dialogue.hpp"
 
 
@@ -39,6 +41,48 @@ TEST(testDialogue, testLinkNodes) {
     testDialogue.linkNodes(0, 1);
 
     ASSERT_EQ(testDialogue.getChoiceCount(), 1);
+}
+
+TEST(testDialogue, testMakeChoice) {
+    Dialogue testDialogue = Dialogue("Head message", "speaker");
+    testDialogue.addNode("New message", "new speaker");
+
+    testDialogue.linkNodes(0, 1);
+    testDialogue.makeChoice(0);
+
+    ASSERT_EQ(testDialogue.getMessage(), "New message");
+    ASSERT_EQ(testDialogue.getSpeaker(), "new speaker");
+}
+
+TEST(testDialogue, testReset) {
+    Dialogue testDialogue = Dialogue("Head message", "speaker");
+    testDialogue.addNode("New message", "new speaker");
+
+    testDialogue.linkNodes(0, 1);
+    testDialogue.makeChoice(0);     // Current node is now Node 1
+    testDialogue.reset();           // Current node is now Node 0 (head)
+
+    ASSERT_EQ(testDialogue.getMessage(), "Head message");
+    ASSERT_EQ(testDialogue.getSpeaker(), "speaker");
+}
+
+TEST(testDialogue, testAddTerminalChoice) {
+    Dialogue testDialogue = Dialogue("Head message", "speaker");
+
+    testDialogue.addTerminalChoice(0);
+    std::vector<std::string> expectedChoiceMessages = {"[END]"};
+
+    ASSERT_EQ(testDialogue.getChoiceCount(), 1);
+    ASSERT_EQ(testDialogue.getChoiceMessages(), expectedChoiceMessages);
+}
+
+TEST(testDialogue, testIsDone) {
+    Dialogue testDialogue = Dialogue("Head message", "speaker");
+
+    testDialogue.addTerminalChoice(0);
+    testDialogue.makeChoice(0);
+
+    ASSERT_TRUE(testDialogue.isDone());
 }
 
 
