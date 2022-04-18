51a79f7c3eee30db05a7770dd907f21242981c98
Marcin <marcinlojek14@gmail.com>
2022-04-18 17:50:52 +0200

    Rename Node members and methods, rearranged methods

--- src/node.hpp
+++ src/node.hpp
@@ -4,6 +4,7 @@
 
 #include <vector>
 #include <string>
+#include <tuple>
 
 
 namespace rpgDialogue {
@@ -11,35 +12,40 @@ namespace rpgDialogue {
     private:
         std::string message_;
         std::string speaker_;
-        std::vector<Node*> nextNodes_;
-        bool visited_;
+        std::vector<Node*> choices_;
+        bool seen_;
 
     public:
         Node(std::string message, std::string speaker);
 
+        // Message getters:
         std::string getMessage() const;
         std::string getSpeaker() const;
         std::string printMessage() const;
 
-        int getChoiceCount() const;
-        Node* getNext(int choiceNo);
-        bool isVisited() const;
-        std::vector<bool> getNextVisited() const;
+        // Message setters:
+        void setMessage(std::string newMessage);
+        void setSpeaker(std::string newSpeaker);
 
+        // Choices manipulation:
+        void addChoice(Node* nextNode);
+        int getChoiceCount() const;
+        Node* makeChoice(int choiceNo);
+        void clearChoices();
+
+        // Choice messages getters:
         std::vector<std::string> getChoiceMessages() const;
         std::vector<std::string> getChoiceMessagesNumbered() const;
         std::vector<std::string> printChoiceMessages() const;
         std::vector<std::string> printChoiceMessagesNumbered() const;
-
+        // These two are just grabage names
         std::vector<std::tuple<std::string, bool>> getChoiceMessagesAndVisited() const;
         std::vector<std::tuple<std::string, bool>> printChoiceMessagesAndVisited() const;
 
-        void setMessage(std::string newMessage);
-        void setSpeaker(std::string newSpeaker);
-        void addChoice(Node* nextNode);
-        void setVisited(bool newVal);
-
-        void resetChoices();
+        // Visited info:
+        bool isSeen() const;
+        void setSeen(bool newVal);
+        std::vector<bool> getNextVisited() const;       // rename asap
     };
 }
 
