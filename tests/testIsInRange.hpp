6c18792ad3e7c2dc3bf5c2645df0040c5de3f29e
Marcin <marcinlojek14@gmail.com>
2022-04-20 09:34:49 +0200

    Add first 8 isInRange unit tests

--- tests/testIsInRange.hpp
+++ tests/testIsInRange.hpp
@@ -9,4 +9,37 @@
 using namespace rpgDialogue;
 
 
+TEST(testIsInRange, testBelowRange) {
+    ASSERT_FALSE(isInRange(2, 5, 7));
+}
+
+TEST(testIsInRange, testEqualRangeStart) {
+    ASSERT_TRUE(isInRange(-4, -4, 5));
+}
+
+TEST(testIsInRange, testInRange) {
+    ASSERT_TRUE(isInRange(3, 1, 7));
+}
+
+TEST(testIsInRange, testEqualRangeStop) {
+    ASSERT_TRUE(isInRange(5, -2, 5));
+}
+
+TEST(testIsInRange, testAboveRange) {
+    ASSERT_FALSE(isInRange(10, 5, 7));
+}
+
+TEST(testIsInRange, testBelowRangeBoundsEqual) {
+    ASSERT_FALSE(isInRange(2, 5, 5));
+}
+
+TEST(testIsInRange, testInRangeBoundsEqual) {
+    ASSERT_TRUE(isInRange(-2, -2, -2));
+}
+
+TEST(testIsInRange, testOverRangeBoudsEqual) {
+    ASSERT_FALSE(isInRange(40, 7, 7));
+}
+
+
 #endif
