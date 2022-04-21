720c43278194c5da1f357361c92997639d75d2d6
Marcin <marcinlojek14@gmail.com>
2022-04-21 21:04:09 +0200

    Add testDialogue respect unit tests

--- tests/testDialogue.hpp
+++ tests/testDialogue.hpp
@@ -273,5 +273,52 @@ TEST(testDialogue, testIsDoneFalse) {
     ASSERT_FALSE(testDialogue.isDone());
 }
 
+// Respect:
+TEST(testDialogue, testRespectConstructorDefault) {
+    Dialogue testDialogue("Head message", "speaker");
+
+    ASSERT_EQ(testDialogue.getRespect(), 0);
+}
+
+TEST(testDialogue, testRespectConstructorValue) {
+    Dialogue testDialogue("Head message", "speaker", -100);
+
+    ASSERT_EQ(testDialogue.getRespect(), -100);
+}
+
+TEST(testDialogue, testRespectAccumulation) {
+    Dialogue testDialogue("Head message", "speaker", -100);
+
+    testDialogue.addNode("New message", "new speaker", 30);
+    testDialogue.addNode("New new message", "new new speaker", 77);
+
+    testDialogue.linkNodes(0, 1);
+    testDialogue.linkNodes(1, 2);
+
+    ASSERT_EQ(testDialogue.getRespect(), -100);
+
+    testDialogue.makeChoice(0);
+    testDialogue.makeChoice(0);
+
+    ASSERT_EQ(testDialogue.getRespect(), 7);
+}
+
+TEST(testDialogue, testRespectAccumulationAndReset) {
+    Dialogue testDialogue("Head message", "speaker", -100);
+
+    testDialogue.addNode("New message", "new speaker", 30);
+    testDialogue.addNode("New new message", "new new speaker", 77);
+
+    testDialogue.linkNodes(0, 1);
+    testDialogue.linkNodes(1, 2);
+
+    testDialogue.makeChoice(0);
+    testDialogue.makeChoice(0);
+
+    testDialogue.reset();
+
+    ASSERT_EQ(testDialogue.getRespect(), -100);
+}
+
 
 #endif
