83be70264cec81fd4462d67f50edee0e1890380c
Marcin <marcinlojek14@gmail.com>
2022-05-04 19:05:51 +0200

    Disable Node class

--- src/node.cpp
+++ src/node.cpp
@@ -10,180 +10,180 @@ namespace rpgDialogue {
         return rangeEnd >= rangeStart && number >= rangeStart && number <= rangeEnd;
     }
 
-    Node::Node(std::string message, std::string speaker, int respectGain, int infoGain)
-        : m_message(message), m_speaker(speaker), m_respectGain(respectGain), m_seen(false), m_infoGain(infoGain) {}
+    // Node::Node(std::string message, std::string speaker, int respectGain, int infoGain)
+    //     : m_message(message), m_speaker(speaker), m_respectGain(respectGain), m_seen(false), m_infoGain(infoGain) {}
 
-    // Message getters:
-    std::string Node::getMessage() const {
-        return m_message;
-    }
+    // // Message getters:
+    // std::string Node::getMessage() const {
+    //     return m_message;
+    // }
 
-    std::string Node::getSpeaker() const {
-        return m_speaker;
-    }
+    // std::string Node::getSpeaker() const {
+    //     return m_speaker;
+    // }
 
-    std::string Node::printMessage() const {
-        return m_speaker + ": " + m_message;
-    }
+    // std::string Node::printMessage() const {
+    //     return m_speaker + ": " + m_message;
+    // }
 
-    // Message setters:
-    void Node::setMessage(std::string newMessage) {
-        m_message = newMessage;
-    }
+    // // Message setters:
+    // void Node::setMessage(std::string newMessage) {
+    //     m_message = newMessage;
+    // }
 
-    void Node::setSpeaker(std::string newSpeaker) {
-        m_speaker = newSpeaker;
-    }
+    // void Node::setSpeaker(std::string newSpeaker) {
+    //     m_speaker = newSpeaker;
+    // }
 
-    // Choices manipulation:
-    void Node::addChoice(Node* nextNode) {
-        if (nextNode != this)
-            m_choices.push_back(nextNode);
-    }
+    // // Choices manipulation:
+    // void Node::addChoice(Node* nextNode) {
+    //     if (nextNode != this)
+    //         m_choices.push_back(nextNode);
+    // }
 
-    int Node::getChoiceCount() const {
-        return m_choices.size();
-    }
+    // int Node::getChoiceCount() const {
+    //     return m_choices.size();
+    // }
 
-    Node* Node::makeChoice(int choiceNo) {
-        if (isInRange(choiceNo, 0, m_choices.size() - 1))
-            return m_choices[choiceNo];
-        else
-            return this;
-    }
+    // Node* Node::makeChoice(int choiceNo) {
+    //     if (isInRange(choiceNo, 0, m_choices.size() - 1))
+    //         return m_choices[choiceNo];
+    //     else
+    //         return this;
+    // }
 
-    void Node::clearChoices() {
-        m_choices.clear();
-    }
+    // void Node::clearChoices() {
+    //     m_choices.clear();
+    // }
 
-    // Choice messages getters:
-    std::vector<std::string> Node::getChoiceMessages() const {
-        std::vector<std::string> result;
+    // // Choice messages getters:
+    // std::vector<std::string> Node::getChoiceMessages() const {
+    //     std::vector<std::string> result;
 
-        for (Node* choice : m_choices) {
-            if (choice == nullptr)
-                result.push_back(END_MESSAGE);
-            else
-                result.push_back(choice->getMessage());
-        }
+    //     for (Node* choice : m_choices) {
+    //         if (choice == nullptr)
+    //             result.push_back(END_MESSAGE);
+    //         else
+    //             result.push_back(choice->getMessage());
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
-    std::vector<std::string> Node::getChoiceMessagesNumbered() const {
-        std::vector<std::string> result;
-        int index = 0;
+    // std::vector<std::string> Node::getChoiceMessagesNumbered() const {
+    //     std::vector<std::string> result;
+    //     int index = 0;
 
-        for (Node* choice : m_choices) {
-            if (choice == nullptr)
-                result.push_back(std::to_string(index) + ") " + END_MESSAGE);
-            else
-                result.push_back(std::to_string(index) + ") " + choice->getMessage());
+    //     for (Node* choice : m_choices) {
+    //         if (choice == nullptr)
+    //             result.push_back(std::to_string(index) + ") " + END_MESSAGE);
+    //         else
+    //             result.push_back(std::to_string(index) + ") " + choice->getMessage());
 
-            index++;
-        }
+    //         index++;
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
-    std::vector<std::string> Node::printChoiceMessages() const {
-        std::vector<std::string> result;
+    // std::vector<std::string> Node::printChoiceMessages() const {
+    //     std::vector<std::string> result;
 
-        for (Node* choice : m_choices) {
-            if (choice == nullptr)
-                result.push_back(END_MESSAGE);
-            else
-                result.push_back(choice->printMessage());
-        }
+    //     for (Node* choice : m_choices) {
+    //         if (choice == nullptr)
+    //             result.push_back(END_MESSAGE);
+    //         else
+    //             result.push_back(choice->printMessage());
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
-    std::vector<std::string> Node::printChoiceMessagesNumbered() const {
-        std::vector<std::string> result;
-        int index = 0;
+    // std::vector<std::string> Node::printChoiceMessagesNumbered() const {
+    //     std::vector<std::string> result;
+    //     int index = 0;
 
-        for (Node* choice : m_choices) {
-            if (choice == nullptr)
-                result.push_back(std::to_string(index) + ") " + END_MESSAGE);
-            else
-                result.push_back(std::to_string(index) + ") " + choice->printMessage());
+    //     for (Node* choice : m_choices) {
+    //         if (choice == nullptr)
+    //             result.push_back(std::to_string(index) + ") " + END_MESSAGE);
+    //         else
+    //             result.push_back(std::to_string(index) + ") " + choice->printMessage());
 
-            index++;
-        }
+    //         index++;
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
-    std::vector<std::tuple<std::string, bool>> Node::getChoicesInfo() const {
-        std::vector<std::tuple<std::string, bool>> result;
+    // std::vector<std::tuple<std::string, bool>> Node::getChoicesInfo() const {
+    //     std::vector<std::tuple<std::string, bool>> result;
 
-        for (Node* choice : m_choices) {
-            if (choice == nullptr) {
-                result.push_back({END_MESSAGE, false});
-            }
-            else {
-                result.push_back({choice->getMessage(), choice->isSeen()});
-            }
-        }
+    //     for (Node* choice : m_choices) {
+    //         if (choice == nullptr) {
+    //             result.push_back({END_MESSAGE, false});
+    //         }
+    //         else {
+    //             result.push_back({choice->getMessage(), choice->isSeen()});
+    //         }
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
-    std::vector<std::tuple<std::string, bool>> Node::printChoicesInfo() const {
-        std::vector<std::tuple<std::string, bool>> result;
+    // std::vector<std::tuple<std::string, bool>> Node::printChoicesInfo() const {
+    //     std::vector<std::tuple<std::string, bool>> result;
 
-        for (Node* choice : m_choices) {
-            if (choice == nullptr) {
-                result.push_back({END_MESSAGE, false});
-            }
-            else {
-                result.push_back({choice->printMessage(), choice->isSeen()});
-            }
-        }
+    //     for (Node* choice : m_choices) {
+    //         if (choice == nullptr) {
+    //             result.push_back({END_MESSAGE, false});
+    //         }
+    //         else {
+    //             result.push_back({choice->printMessage(), choice->isSeen()});
+    //         }
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
-    // Seen info:
-    bool Node::isSeen() const {
-        return m_seen;
-    }
+    // // Seen info:
+    // bool Node::isSeen() const {
+    //     return m_seen;
+    // }
 
-    void Node::setSeen(bool newVal) {
-        m_seen = newVal;
-    }
+    // void Node::setSeen(bool newVal) {
+    //     m_seen = newVal;
+    // }
 
-    std::vector<bool> Node::getChoicesSeen() const {
-        std::vector<bool> result;
+    // std::vector<bool> Node::getChoicesSeen() const {
+    //     std::vector<bool> result;
 
-        for (Node* choice : m_choices) {
-            if (choice == nullptr) {
-                result.push_back(false);
-            }
-            else {
-                result.push_back(choice->isSeen());
-            }
-        }
+    //     for (Node* choice : m_choices) {
+    //         if (choice == nullptr) {
+    //             result.push_back(false);
+    //         }
+    //         else {
+    //             result.push_back(choice->isSeen());
+    //         }
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
-    // Respect gain:
-    void Node::setRespectGain(int newVal) {
-        m_respectGain = newVal;
-    }
+    // // Respect gain:
+    // void Node::setRespectGain(int newVal) {
+    //     m_respectGain = newVal;
+    // }
 
-    int Node::getRespectGain() const {
-        return m_respectGain;
-    }
+    // int Node::getRespectGain() const {
+    //     return m_respectGain;
+    // }
 
-    // Info gain:
-    void Node::setInfoGain(int newVal) {
-        m_infoGain = newVal;
-    }
+    // // Info gain:
+    // void Node::setInfoGain(int newVal) {
+    //     m_infoGain = newVal;
+    // }
 
-    int Node::getInfoGain() const {
-        return m_infoGain;
-    }
+    // int Node::getInfoGain() const {
+    //     return m_infoGain;
+    // }
 }
--- src/node.hpp
+++ src/node.hpp
@@ -10,54 +10,54 @@
 namespace rpgDialogue {
     bool isInRange(int number, int rangeStart, int rangeEnd);
 
-    class Node {
-    private:
-        std::string m_message;
-        std::string m_speaker;
-        std::vector<Node*> m_choices;
-        bool m_seen;
-        int m_respectGain;
-        int m_infoGain;
+    // class Node {
+    // private:
+    //     std::string m_message;
+    //     std::string m_speaker;
+    //     std::vector<Node*> m_choices;
+    //     bool m_seen;
+    //     int m_respectGain;
+    //     int m_infoGain;
 
-    public:
-        Node(std::string message, std::string speaker, int respectGain = 0, int infoGain = 0);
+    // public:
+    //     Node(std::string message, std::string speaker, int respectGain = 0, int infoGain = 0);
 
-        // Message getters:
-        std::string getMessage() const;
-        std::string getSpeaker() const;
-        std::string printMessage() const;
+    //     // Message getters:
+    //     std::string getMessage() const;
+    //     std::string getSpeaker() const;
+    //     std::string printMessage() const;
 
-        // Message setters:
-        void setMessage(std::string newMessage);
-        void setSpeaker(std::string newSpeaker);
+    //     // Message setters:
+    //     void setMessage(std::string newMessage);
+    //     void setSpeaker(std::string newSpeaker);
 
-        // Choices manipulation:
-        void addChoice(Node* nextNode);
-        int getChoiceCount() const;
-        Node* makeChoice(int choiceNo);
-        void clearChoices();
+    //     // Choices manipulation:
+    //     void addChoice(Node* nextNode);
+    //     int getChoiceCount() const;
+    //     Node* makeChoice(int choiceNo);
+    //     void clearChoices();
 
-        // Choice messages getters:
-        std::vector<std::string> getChoiceMessages() const;
-        std::vector<std::string> getChoiceMessagesNumbered() const;
-        std::vector<std::string> printChoiceMessages() const;
-        std::vector<std::string> printChoiceMessagesNumbered() const;
-        std::vector<std::tuple<std::string, bool>> getChoicesInfo() const;
-        std::vector<std::tuple<std::string, bool>> printChoicesInfo() const;
+    //     // Choice messages getters:
+    //     std::vector<std::string> getChoiceMessages() const;
+    //     std::vector<std::string> getChoiceMessagesNumbered() const;
+    //     std::vector<std::string> printChoiceMessages() const;
+    //     std::vector<std::string> printChoiceMessagesNumbered() const;
+    //     std::vector<std::tuple<std::string, bool>> getChoicesInfo() const;
+    //     std::vector<std::tuple<std::string, bool>> printChoicesInfo() const;
 
-        // Seen info:
-        bool isSeen() const;
-        void setSeen(bool newVal);
-        std::vector<bool> getChoicesSeen() const;
+    //     // Seen info:
+    //     bool isSeen() const;
+    //     void setSeen(bool newVal);
+    //     std::vector<bool> getChoicesSeen() const;
 
-        // Respect gain:
-        void setRespectGain(int newVal);
-        int getRespectGain() const;
+    //     // Respect gain:
+    //     void setRespectGain(int newVal);
+    //     int getRespectGain() const;
 
-        // Info gain:
-        void setInfoGain(int newVal);
-        int getInfoGain() const;
-    };
+    //     // Info gain:
+    //     void setInfoGain(int newVal);
+    //     int getInfoGain() const;
+    // };
 }
 
 
